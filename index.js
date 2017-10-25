87c93143edf5109a5a18741323578b2424e7efe9
Kingston <yeelyn.lim@outlook.com>
2017-10-25 21:34:14 +0800

    fixes node server

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ var express = require('express'),
 app.use(express.static(path.join(__dirname, './public')))
 
 // Routes
-app.use('/', (req,res) => {
+app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
+if (env == 'production') {
+  app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''))
+    }
+    next();
+  });
+}
 
 var server = app.listen(process.env.PORT || 3000)
